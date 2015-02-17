12ee58257e614545a47202195c4517a9b466d677
Alexander Trauzzi <alexander@picticipate.com>
2015-02-17 09:49:44 -0600

    Capture environment variables for Laravel to use command line.

--- images/php-cli/entrypoint.sh
+++ images/php-cli/entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-echo "Configuring SSH Environment for `whoami`..."
+echo "Configuring environment for `whoami`..."
+export DB_HOST = $POSTGRES_PORT_5432_TCP_ADDR
+export DB_DATABASE = $POSTGRES_DATABASE
+export POSTGRES_PORT = $POSTGRES_PORT_5432_TCP_PORT
+
+export REDIS_HOST = $REDIS_PORT_6379_TCP_ADDR
+export REDIS_PORT = $REDIS_PORT_6379_TCP_PORT
+
 rm -rf ~/.ssh
 cp -r ~/.host-ssh ~/.ssh
 chown -R root:root ~/.ssh
