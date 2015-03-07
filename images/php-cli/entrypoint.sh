2a2a62563959d6c91bc5c290fa7a17ee1ff63284
Alexander Trauzzi <atrauzzi@gmail.com>
2015-03-06 23:15:53 -0600

    Updated cli entrypoint to dummy-in a user.

--- images/php-cli/entrypoint.sh
+++ images/php-cli/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-echo "Configuring environment for `whoami`..."
+echo "Configuring environment..."
+
+export USER=$USER
+export UID=$UID
+
 export APP_DEBUG=$APP_DEBUG
 export DB_HOST=$POSTGRES_PORT_5432_TCP_ADDR
 export POSTGRES_PORT=$POSTGRES_PORT_5432_TCP_PORT
@@ -8,12 +12,19 @@ export DB_DATABASE=$POSTGRES_DATABASE
 export DB_USERNAME=$POSTGRES_USER
 export DB_PASSWORD=$POSTGRES_PASSWORD
 
+export CACHE_DRIVER=$CACHE_DRIVER
 export REDIS_HOST=$REDIS_PORT_6379_TCP_ADDR
 export REDIS_PORT=$REDIS_PORT_6379_TCP_PORT
 
-rm -rf ~/.ssh
-cp -r ~/.host-ssh ~/.ssh
-chown -R root:root ~/.ssh
-chmod -R 400 ~/.ssh
+useradd --uid $UID $USER
+echo "Running as $USER ($UID)"
 
-"$@"
+rm -rf /home/$USER/.ssh
+cp -r /home/$USER/.host-ssh /home/$USER/.ssh
+chown -R $USER:users /home/$USER/.ssh
+chmod -R 400 /home/$USER/.ssh
+
+echo "Debug: $APP_DEBUG"
+echo
+
+su $USER -m -c "$@"
